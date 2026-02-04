06df5e7aa6815b090eeb8a6c32dc499919387e57
SteveZhang <871506263@qq.com>
2026-02-04 11:05:31 +0800

    feat: add base effect and topology stylesheets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 // import './index.css' // Optional global styles
+import './styles/effects.css'
+import './styles/topology.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
